eb97c45e4899057d1ee9b6b0c30c9c48c4d66984
Pedro Felipe Froes <pedrofelipefroes@gmail.com>
2018-08-28 21:33:43 -0300

    feat/setup: Fixes to serve HTML to repo, and updating Gulp with Clean.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const cleanDir = require('gulp-clean');
 const cleanCSS = require('gulp-clean-css');
 const flatten = require('gulp-flatten');
 const gulp = require('gulp');
@@ -7,9 +8,15 @@ const mergeJSON = require('gulp-merge-json');
 const mustache = require('gulp-mustache');
 const sass = require('gulp-sass');
 
+gulp.task('clean', function () {
+  return gulp
+    .src(['build/', 'work/', '*.html', '*.css'], { allowEmpty: true, read: false })
+    .pipe(cleanDir());
+})
+
 gulp.task('compile-mustache', function () {
   return gulp
-    .src('build/*.mustache')
+    .src('build/**/*.mustache')
     .pipe(mustache(
       'build/index.json',
       { extension: '.html' },
@@ -26,10 +33,16 @@ gulp.task('compile-sass', function () {
     .pipe(gulp.dest('.'));
 });
 
+gulp.task('serve-index', function () {
+  return gulp
+    .src('src/index.mustache')
+    .pipe(gulp.dest('build'));
+})
+
 gulp.task('serve-views', function () {
   return gulp
     .src(['src/views/*/*.mustache'])
-    .pipe(flatten({ includeParents: 0 }))
+    .pipe(flatten({ newPath: 'work/' }))
     .pipe(gulp.dest('build'));
 });
 
@@ -42,10 +55,9 @@ gulp.task('serve-json', function () {
     .pipe(gulp.dest('build'));
 });
 
+const clean = gulp.parallel('clean');
 const build = gulp.parallel('compile-mustache', 'compile-sass');
-const serve = gulp.parallel('serve-views', 'serve-json');
-const start = gulp.series(serve, build);
+const serve = gulp.parallel('serve-index', 'serve-views', 'serve-json');
+const start = gulp.series(clean, serve, build);
 
-gulp.task('build', build);
-gulp.task('serve', serve);
 gulp.task('default', start);
